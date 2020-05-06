d1eff18aed76258814a57e65d624b6297cc69697
ma7modGamel <mahmoudgamel10@gmail.com>
2020-05-06 11:11:36 +0200

    CREEATE mg

---
+++ app/src/main/java/com/mgh/tyar3tayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mgh.tyar3tayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
